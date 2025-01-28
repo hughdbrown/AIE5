2f517671d9ba10a01faa860cf2bb1b7a7023e7d9
Hugh Brown <hughdbrown@yahoo.com>
2025-01-28 14:50:07 -0700

    Add PDF class

--- 02_Embeddings_and_RAG/aimakerspace/text_utils.py
+++ 02_Embeddings_and_RAG/aimakerspace/text_utils.py
@@ -1,5 +1,12 @@
+import logging
 import os
+from pathlib import Path
 from typing import List
+
+from PyPDF2 import PdfReader
+
+
+logger = logging.getLogger(__name__)
 
 
 class TextFileLoader:
@@ -30,6 +37,41 @@ class TextFileLoader:
                         os.path.join(root, file), "r", encoding=self.encoding
                     ) as f:
                         self.documents.append(f.read())
+
+    def load_documents(self):
+        self.load()
+        return self.documents
+
+
+class PDFFileLoader:
+    """Load files as PDF, extract text."""
+
+    def __init__(self, path: str):
+        self.documents = []
+        self.path = path
+
+    def load(self):
+        logger.info(f"load({self.path})")
+        p = Path(self.path)
+        if p.is_dir():
+            self.load_directory()
+        elif p.is_file() and p.suffix == ".pdf":
+            self.load_file()
+
+    def _load_pdf(self, filename):
+        logger.info(f"Loading PDF '{filename}'")
+        reader = PdfReader(filename)
+        return "".join([page.extract_text() for page in reader.pages])
+
+    def load_file(self):
+        logger.info(f"load_file('{self.path}')")
+        self.documents.append(self._load_pdf(self.path))
+
+    def load_directory(self):
+        p = Path(self.path)
+        logger.info(f"load_directory('{str(p)}')")
+        for filename in p.rglob("*.pdf"):
+            self.documents.append(self._load_pdf(filename))
 
     def load_documents(self):
         self.load()
